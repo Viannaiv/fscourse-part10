838fa6af6821ab42e5d4f1c2cc95f89d38811b51
Viannaiv <viva95@windowslive.com>
2023-06-23 17:03:25 +0300

    Exercise 10.18

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -49,6 +49,18 @@ const validationSchema = yup.object().shape({
     .required('Password is required'),
 })
 
+export const SignInContainer = ({ onSubmit }) => {
+	return (
+		<Formik
+			initialValues={initialValues}
+			onSubmit={onSubmit}
+			validationSchema={validationSchema}
+		>
+			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+		</Formik>
+	)
+}
+
 const SignIn = () => {
 	const [signIn] = useSignIn()
 	const navigate = useNavigate()
@@ -65,15 +77,7 @@ const SignIn = () => {
 		}
 	}
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			onSubmit={onSubmit}
-			validationSchema={validationSchema}
-		>
-			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-		</Formik>
-	)
+	return <SignInContainer onSubmit={onSubmit} />
 }
 
 export default SignIn
